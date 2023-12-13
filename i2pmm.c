2ff06dd538a387eea866ea21e458943b2d49779c
wangchaosun <2962056732@qq.com>
2023-12-13 21:08:18 +0800

    register hook ok

--- i2pmm.c
+++ i2pmm.c
@@ -1,22 +1,59 @@
 #include <linux/init.h> /* Needed for the macros */
 #include <linux/kernel.h> 
 #include <linux/module.h> /* Needed by all modules */
- 
-static int __init hello_init(void)
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+
+/**********function define begin**********/
+unsigned int hook_proc( void * priv, struct sk_buff * skb, 
+		        const struct nf_hook_state * state );
+
+/**********function define end************/
+
+static struct nf_hook_ops nf_local_in = {
+	.hook = hook_proc,
+	.priv = NULL,
+	.pf = PF_INET,
+	.hooknum = NF_INET_LOCAL_OUT ,
+	.priority = NF_IP_PRI_FIRST ,
+};
+
+/*
+static struct nf_hook_ops nf_local_in = {
+	.hook = 
+	.pf = PF_INET,
+	.hooknum = NF_INET_LOCAL_IN ,
+	.priority = NF_IP_PRI_FIRST ,
+	.owner = THIS_MODULE
+};
+*/
+
+unsigned int hook_proc( void * priv, struct sk_buff * skb, 
+		        const struct nf_hook_state * state ){
+    printk(KERN_INFO"hook_proc");
+    return NF_ACCEPT;
+}
+
+
+static int __init i2pmm_init(void)
 {
-    printk(KERN_INFO "Hello, world\n");
+    printk(KERN_INFO "init i2pmm module !!!\n");
+    if( nf_register_net_hook( &init_net, &nf_local_in ) ){
+	printk(KERN_INFO"nf_register_hook() failed !\n");
+    }else{
+	printk(KERN_INFO"nf_register_hook() succeed !\n");
+    }
     return 0;
 }
  
-static void __exit hello_exit(void)
+static void __exit i2pmm_exit(void)
 {
-    printk(KERN_INFO "Goodbye, world\n");
+    printk(KERN_INFO "exit i2pmm module !!!\n");
+    nf_unregister_net_hook(&init_net,  &nf_local_in );
 }
  
-module_init(hello_init);
-module_exit(hello_exit);
-
-
+module_init(i2pmm_init);
+module_exit(i2pmm_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("yyrwkk");
